cdff60d306d8bcefca49f8c3a08ea941628ee923
vinnegar <vanshsharma.may2007@gmail.com>
2026-06-26 00:45:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,13 +6,6 @@ const {Client}=require('/var/lib/ghost/versions/5.130.6/node_modules/pg');
 const c=new Client({connectionString:process.env.database__connection__connectionString,ssl:{rejectUnauthorized:false}});
 c.connect()
   .then(()=>c.query('DROP TABLE IF EXISTS migrations_lock CASCADE'))
-  .then(()=>c.query('DROP TABLE IF EXISTS migrations CASCADE'))
-  .then(()=>c.query(\`CREATE TABLE migrations (
-    id serial PRIMARY KEY,
-    name varchar(255),
-    version varchar(255),
-    \"currentVersion\" varchar(255)
-  )\`))
   .then(()=>c.query(\`CREATE TABLE migrations_lock (
     lock_key varchar(255) NOT NULL,
     locked integer NOT NULL DEFAULT 0,
