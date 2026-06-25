2ebc312964ba4720a2bfb596ff283d18bc89129f
vinnegar <vanshsharma.may2007@gmail.com>
2026-06-26 00:15:31 +0530

    Update database cleanup message in start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ const {Client}=require('/var/lib/ghost/versions/5.130.6/node_modules/pg');
 const c=new Client({connectionString:process.env.database__connection__connectionString,ssl:{rejectUnauthorized:false}});
 c.connect()
  .then(()=>c.query('DROP TABLE IF EXISTS migrations_lock CASCADE'))
- .then(()=>{console.log('DB cleaned');return c.end();})
- .catch((e)=>{console.log('Note:',e.message);return c.end();});
+ .then(()=>{console.log('Ready');return c.end();})
+ .catch((e)=>{console.log('Note:',e.message);c.end();});
 " || true
 exec docker-entrypoint.sh node current/index.js
