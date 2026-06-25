65b9140a39448998269512ce45cb80472c9c2761
vinnegar <vanshsharma.may2007@gmail.com>
2026-06-26 00:20:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,9 @@ const {Client}=require('/var/lib/ghost/versions/5.130.6/node_modules/pg');
 const c=new Client({connectionString:process.env.database__connection__connectionString,ssl:{rejectUnauthorized:false}});
 c.connect()
  .then(()=>c.query('DROP TABLE IF EXISTS migrations_lock CASCADE'))
- .then(()=>{console.log('Ready');return c.end();})
- .catch((e)=>{console.log('Note:',e.message);c.end();});
+ .then(()=>c.query('CREATE TABLE migrations_lock (lock_key varchar(255) NOT NULL, locked integer NOT NULL DEFAULT 0, acquired_at varchar(255), released_at varchar(255))'))
+ .then(()=>c.query(\"INSERT INTO migrations_lock (lock_key, locked) VALUES ('km01', 0)\"))
+ .then(()=>{console.log('OK: table ready, no PK');return c.end();})
+ .catch((e)=>{console.log('note:',e.message);return c.end();});
 " || true
 exec docker-entrypoint.sh node current/index.js
